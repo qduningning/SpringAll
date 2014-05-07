60898cd55e9d17eae544ca7b199c2f5fd8da7ddb
qduningning <zhangningning2012@163.com>
2014-05-08 00:18:49 +0800

    add sql

---
+++ spring_user.sql
@@ -0,0 +1,29 @@
+/*
+SQLyog v10.2 
+MySQL - 5.5.24-log : Database - mysql
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `spring_user` */
+
+CREATE TABLE `spring_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `age` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `spring_user` */
+
+insert  into `spring_user`(`id`,`name`,`age`) values (2,'znn',21),(3,'adminn1',22),(4,'adminn',22);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
